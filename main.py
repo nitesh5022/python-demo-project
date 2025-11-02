52dc3b7920b97667b0652a14a3f3cd6752bd56be
Nitesh kumar <nitesh50227@gmail.com>
2025-11-02 09:57:18 +0530

    initial Python Project

---
+++ main.py
@@ -0,0 +1,6 @@
+def greet_user(name):
+    return f"Hello {name}, Welcome to Python GitHub Project!"
+
+if __name__ == "__main__":
+    your_name = input("Enter your name: ")
+    print(greet_user(your_name))
